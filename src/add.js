7b33b2f375f77f99008cd4deee5a9d03cb0ec52a
haresh-samnani <haresh.samnani@gmail.com>
2024-11-16 13:34:31 +0400

    Create sum numbers test

--- src/add.js
+++ src/add.js
@@ -1,5 +1,10 @@
 const add = (numbers) => {
   if (!numbers) return 0; // 0 for an empty string
+
+  return numbers
+    .split(",")
+    .map((num) => parseFloat(num.trim())) // convert to numbers
+    .reduce((sum, num) => sum + num, 0); // sum numbers
 };
 
 export default add;
